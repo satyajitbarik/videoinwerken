d1ad9cc7dbe39c60dde2d535bf88f5ef466abcc8
Simon Jacobs <s.jacobs@student.tue.nl>
2020-06-15 22:28:33 +0200

    get courses for manager_id1

--- django_backend/videoinwerken_manager/views.py
+++ django_backend/videoinwerken_manager/views.py
@@ -3,14 +3,15 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .models import Course
+#from ..user_profile.models import User
 
 # Create your views here.
 from .serializers import CourseSerializer
 
 class ListAllCourses(generics.ListCreateAPIView):
-    queryset = Course.objects.all()
+    #queryset = Course.objects.all()
+    queryset = Course.objects.filter(manager_id=1)
     serializer_class = CourseSerializer
-
 
 #class ListCoursesOfManager(generics.ListCreateAPIView):
    # queryset = Course.objects.get(manager)
