d98ce7a54275ccb0e2315fa1b1ac3322d4e6e202
Simon Jacobs <s.jacobs@student.tue.nl>
2020-06-19 23:33:25 +0200

    Course class to function success

--- react_frontend/src/components/Manager/Courses/Course.js
+++ react_frontend/src/components/Manager/Courses/Course.js
@@ -1,37 +1,23 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { AuthUrls } from "../../../constants/urls";
 import { Button } from "@material-ui/core";
-
 import { getUserToken1 } from "../../../utils/authUtils";
 import CourseCreate from "./CourseCreate";
 import CourseEdit from "./CourseEdit";
-
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableRow from "@material-ui/core/TableRow";
-import Checkbox from "@material-ui/core/Checkbox";
 
-class Course extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      detailItem: null,
-      showModal: false,
-      coursesList: [],
-      activeItemAdd: null,
-      current_user: {},
-    };
-  }
+function Course() {
+  const [detailItem, setDetailItem] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [coursesList, setCoursesList] = useState([]);
+  const [activeItemAdd, setActiveItemAdd] = useState(null);
+  const [current_user, setCurrent_user] = useState({});
 
-  // This is called upon finishing loading
-  componentDidMount() {
-    this.getCurrentUser();
-    //this.refreshList(); // This is now called in getCurrentUser.
-  }
-
-  getCurrentUser = () => {
+  const getCurrentUser = () => {
     axios
       .get(AuthUrls.USER_PROFILE, {
         headers: {
@@ -39,16 +25,16 @@ class Course extends Component {
         },
       })
       .then((response) => {
-        this.setState({ current_user: response.data });
-        console.log(this.state.current_user);
-        this.refreshList();
+        setCurrent_user(response.data);
+        console.log(current_user);
+        refreshList();
       })
       .catch((error) => {
         console.log(error.response.data);
       });
   };
 
-  refreshList = () => {
+  const refreshList = () => {
     const token = getUserToken1();
     if (!token) {
       return;
@@ -59,19 +45,19 @@ class Course extends Component {
           authorization: "Token " + token,
         },
         params: {
-          manager_id: this.state.current_user.pk,
+          manager_id: current_user.pk,
         },
       })
       .then((response) => {
-        this.setState({ coursesList: response.data });
+        setCoursesList(response.data);
       })
       .catch((error) => {
         console.log(error.response.data);
       });
   };
 
-  renderCourses = () => {
-    const courses = this.state.coursesList;
+  const renderCourses = () => {
+    const courses = coursesList;
     //console.log(courses);
     return (
       <Table>
@@ -80,8 +66,7 @@ class Course extends Component {
             <TableRow
               key={item.id}
               onClick={() => {
-                //this.props.history.push("courses/" + item.id);
-                this.setState({ detailItem: item });
+                setDetailItem(item);
               }}
               style={{ cursor: "pointer" }}
             >
@@ -94,9 +79,8 @@ class Course extends Component {
     );
   };
 
-  createItem = () => {
-    //this.getCurrentUser();
-    const currentUserID = this.state.current_user.pk;
+  const createItem = () => {
+    const currentUserID = current_user.pk;
     console.log("current user id:" + currentUserID);
 
     const item = {
@@ -108,36 +92,32 @@ class Course extends Component {
       video: "",
       manager_id: currentUserID,
     };
-    this.setState({ activeItemAdd: item, showModal: true });
+    setActiveItemAdd(item);
+    setShowModal(true);
   };
 
-  detailItem = (item) => {
-    const currentUserID = this.state.current_user.pk;
+  const detailItem1 = (item) => {
+    const currentUserID = current_user.pk;
     console.log("current user id:" + currentUserID);
-    //this.setState({ activeItem: item, showModal: true });
 
     return (
       <CourseEdit
-        item={this.state.detailItem}
-        onSave={this.handleSubmit} //not needed
-        onClose={this.handleCloseEdit} //not needed possibly
+        item={detailItem}
+        onSave={handleSubmit} //not needed
+        onClose={handleCloseEdit} //not needed possibly
       />
     );
   };
 
-  showModal = () => {
-    this.setState({ showModal: true });
+  const handleCloseAdd = () => {
+    setShowModal(false);
   };
 
-  handleCloseAdd = () => {
-    this.setState({ showModal: false });
+  const handleCloseEdit = () => {
+    setDetailItem(null);
   };
 
-  handleCloseEdit = () => {
-    this.setState({ detailItem: null });
-  };
-
-  handleSubmit = (item) => {
+  const handleSubmit = (item) => {
     console.log("handlesubmit");
     console.log(item);
     // Edit item
@@ -148,8 +128,8 @@ class Course extends Component {
             authorization: "Token " + getUserToken1(),
           },
         })
-        .then((response) => this.refreshList());
-      this.handleCloseEdit();
+        .then((response) => refreshList());
+      handleCloseEdit();
       return;
     }
     // Create item
@@ -160,41 +140,38 @@ class Course extends Component {
         },
       })
       .then((response) => {
-        this.refreshList();
-        this.handleCloseAdd();
+        refreshList();
+        handleCloseAdd();
       })
       .catch((error) => {
         console.log(error.response.data);
       });
   };
+  return (
+    <div>
+      <h3>Courses</h3>
+      {renderCourses()}
 
-  render() {
-    return (
-      <div>
-        <h3>Courses</h3>
-        {this.renderCourses()}
+      <Button
+        onClick={createItem}
+        variant="contained"
+        color="primary"
+        style={{ marginTop: 20 }}
+      >
+        Make new course
+      </Button>
+      {showModal ? (
+        <CourseCreate
+          item={activeItemAdd}
+          onSave={handleSubmit}
+          onClose={handleCloseAdd}
+          showModal={showModal}
+        />
+      ) : null}
 
-        <Button
-          onClick={this.createItem}
-          variant="contained"
-          color="primary"
-          style={{ marginTop: 20 }}
-        >
-          Make new course
-        </Button>
-        {this.state.showModal ? (
-          <CourseCreate
-            item={this.state.activeItemAdd}
-            onSave={this.handleSubmit}
-            onClose={this.handleCloseAdd}
-            showModal={this.state.showModal}
-          />
-        ) : null}
-
-        {this.state.detailItem ? this.detailItem(this.state.detailItem) : null}
-      </div>
-    );
-  }
+      {detailItem ? detailItem1(detailItem) : null}
+    </div>
+  );
 }
 
 export default Course;
