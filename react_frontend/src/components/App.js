4f9a6a5ec3eec08d67046e688fb07e540926e71e
Simon Jacobs <s.jacobs@student.tue.nl>
2020-06-20 15:11:33 +0200

    Added theme provider

--- react_frontend/src/components/App.js
+++ react_frontend/src/components/App.js
@@ -1,31 +1,39 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Notifs } from "redux-notifications";
 
 import Header from "./Header";
 import MainContent from "./MainContent";
 
-/*export default class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <Notifs />
-        <Header />
-        <MainContent />
-      </div>
-    );
-  }
-}*/
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
+const theme = createMuiTheme({
+  typography: {
+    subtitle1: {
+      fontSize: 12,
+    },
+    body1: {
+      fontWeight: 500,
+    },
+    button: {
+      //fontStyle: "italic",
+    },
+  },
+});
 
 export default function App() {
   const [authenticated, setAuthenticated] = useState(false);
   return (
-    <div /* className="container"*/>
-      <Notifs />
-      <Header
-        authenticated={authenticated}
-        setAuthenticated={setAuthenticated}
-      />
-      <MainContent />
+    <div>
+      <ThemeProvider theme={theme}>
+        <Notifs />
+        <Header
+          authenticated={authenticated}
+          setAuthenticated={setAuthenticated}
+        />
+        <MainContent />
+      </ThemeProvider>
     </div>
   );
 }
