f48e8bd8ea5ce61ff721d860711ff557f9477537
Simon Jacobs <s.jacobs@student.tue.nl>
2020-06-22 19:05:09 +0200

    apiget added

--- react_frontend/src/utils/utils.js
+++ react_frontend/src/utils/utils.js
@@ -1,7 +1,11 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import React from "react";
 import ListItem from "@material-ui/core/ListItem";
 import { FormControlLabel, Checkbox } from "@material-ui/core";
+import axios from "axios";
+import { getUserToken } from "../../../utils/authUtils";
 
 export function ListItemLink(props) {
   return <ListItem button component="a" {...props} />;
@@ -36,4 +40,25 @@ export function MyEditCheckBox(props) {
       style={{ display: "block", marginTop: 10 }}
     />
   );
+}
+
+//axios
+export function apiGet(url, handleResponse, params) {
+  const token = getUserToken();
+  if (!token) {
+    return;
+  }
+  axios
+    .get(url, {
+      headers: {
+        authorization: "Token " + token,
+      },
+      params,
+    })
+    .then((response) => {
+      handleResponse();
+    })
+    .catch((error) => {
+      console.log(error.response.data);
+    });
 }
