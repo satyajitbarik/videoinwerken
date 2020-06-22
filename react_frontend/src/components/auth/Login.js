78680624f5dd31dc0593779e2661bea4847b6ca9
Simon Jacobs <s.jacobs@student.tue.nl>
2020-06-22 09:11:55 +0200

    Login class to function conversion

--- react_frontend/src/components/auth/Login.js
+++ react_frontend/src/components/auth/Login.js
@@ -9,56 +9,50 @@ import { loginUser } from "../../actions/authActions";
 
 import { Button } from "@material-ui/core";
 
-class Login extends Component {
-  static propTypes = {
-    ...propTypes,
-  };
+function Login(props) {
+  const { handleSubmit, error } = props;
 
-  render() {
-    const { handleSubmit, error } = this.props;
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <h4>Please Log In</h4>
+        <hr />
 
-    return (
-      <div className="container">
-        <form onSubmit={handleSubmit}>
-          <h4>Please Log In</h4>
-          <hr />
+        <fieldset className="form-group">
+          <Field
+            name="email"
+            label="Email"
+            component={renderField}
+            type="text"
+            validate={[required({ message: "This field is required." })]}
+          />
+        </fieldset>
 
-          <fieldset className="form-group">
-            <Field
-              name="email"
-              label="Email"
-              component={renderField}
-              type="text"
-              validate={[required({ message: "This field is required." })]}
-            />
-          </fieldset>
+        <fieldset className="form-group">
+          <Field
+            name="password"
+            label="Password"
+            component={renderField}
+            type="password"
+            validate={[required({ message: "This field is required." })]}
+          />
+        </fieldset>
 
-          <fieldset className="form-group">
-            <Field
-              name="password"
-              label="Password"
-              component={renderField}
-              type="password"
-              validate={[required({ message: "This field is required." })]}
-            />
-          </fieldset>
+        <fieldset className="form-group">
+          {renderError(error)}
 
-          <fieldset className="form-group">
-            {renderError(error)}
+          <Button type="submit" variant="contained" color="primary">
+            Login
+          </Button>
+        </fieldset>
 
-            <Button type="submit" variant="contained" color="primary">
-              Login
-            </Button>
-          </fieldset>
-
-          <p>
-            Not registered yet? <Link to="/signup">Signup Here!</Link>
-          </p>
-          <Link to="/reset_password">Forgot password?</Link>
-        </form>
-      </div>
-    );
-  }
+        <p>
+          Not registered yet? <Link to="/signup">Signup Here!</Link>
+        </p>
+        <Link to="/reset_password">Forgot password?</Link>
+      </form>
+    </div>
+  );
 }
 
 export default reduxForm({
