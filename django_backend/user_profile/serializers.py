db590604db8d677fb94df6306d73011ee0c46443
Simon Jacobs <s.jacobs@student.tue.nl>
2020-06-23 15:33:23 +0200

    all fields working now

--- django_backend/user_profile/serializers.py
+++ django_backend/user_profile/serializers.py
@@ -4,12 +4,8 @@ from rest_auth.serializers import UserDetailsSerializer
 
 class UserSerializer(UserDetailsSerializer):
 
-    #website = serializers.URLField(source="userprofile.website", allow_blank=True, required=False)
-    #about = serializers.CharField(source="userprofile.about", allow_blank=True, required=False)
-
     is_admin = serializers.BooleanField(source="userprofile.is_admin", default=False)
     is_employee = serializers.BooleanField(source="userprofile.is_employee", default=False)
-
     is_manager = serializers.BooleanField(source="userprofile.is_manager", default=False)
     license_expiration_date = serializers.DateField(source="userprofile.license_expiration_date", required=False)
     business_name = serializers.CharField(source="userprofile.business_name", allow_blank=True, required=False)
@@ -21,16 +17,32 @@ class UserSerializer(UserDetailsSerializer):
 
     def update(self, instance, validated_data):
         profile_data = validated_data.pop('userprofile', {})
+        is_admin = profile_data.get('is_admin')
+        is_employee = profile_data.get('is_employee')
+        is_manager = profile_data.get('is_manager')
+        license_expiration_date = profile_data.get('license_expiration_date')
+        business_name = profile_data.get('business_name')
+        billing_address = profile_data.get('billing_address')
         iban = profile_data.get('iban')
-        #is_admin = profile_data.get('is_admin')
-     #   is_employee = profile_data.get('is_employee')
-        #is_manager = profile_data.get('is_manager')
 
         instance = super(UserSerializer, self).update(instance, validated_data)
 
         # get and update user profile
         profile = instance.userprofile
         if profile_data:
+
+            if is_admin:
+                profile.is_admin = is_admin
+            if is_employee:
+                profile.is_employee = is_employee
+            if is_manager:
+                profile.is_manager = is_manager
+            if license_expiration_date:
+                profile.license_expiration_date = license_expiration_date
+            if business_name:
+                profile.business_name = business_name
+            if billing_address:
+                profile.billing_address = billing_address
             if iban:
                 profile.iban = iban
             profile.save()
