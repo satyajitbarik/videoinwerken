9bef082f7b56c3e53926dbf4df353a0973a5d76b
Simon Jacobs <s.jacobs@student.tue.nl>
2020-07-15 17:52:48 +0200

    course question database

--- django_backend/videoinwerken_manager/models.py
+++ django_backend/videoinwerken_manager/models.py
@@ -4,13 +4,28 @@ from django.contrib.auth import get_user_model
 from users.models import CustomUser
 
 class Course(models.Model):
+    manager_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=255, blank=False, null=False)
     description = models.CharField(max_length=255, blank=True, null=True)
     active = models.BooleanField(default=True)
     individual_result = models.BooleanField(default=False) # can employee see individual result per question?
     course_duration = models.CharField(max_length=255, blank=True, null=True)  # different model for this? website url?
     video = models.CharField(max_length=255, blank=True, null=True)  # different model for this? website url?
-    manager_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.title
+
+class CourseQuestion(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
+    question = models.CharField(max_length=255, blank=False, null=False)
+
+class CourseQuestionAnswer(models.Model):
+    course_question = models.ForeignKey(CourseQuestion, on_delete=models.CASCADE, null=True)
+    answer = models.CharField(max_length=255, blank=False, null=False)
+    correct = models.BooleanField(default=False)
+
+class EmployeeQuizQuestion(models.Model):
+    employee = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True)
+    course_question = models.ForeignKey(CourseQuestion, on_delete=models.CASCADE, null=True)
+    attempted = models.BooleanField(default=False)
+    passed = models.BooleanField(default=False)
